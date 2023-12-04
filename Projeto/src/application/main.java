11cc9099caa5ee3953189000390cf13474bef730
Guilherme Ascenção <99231465+GuilhermeAsc@users.noreply.github.com>
2023-12-04 15:01:21 -0300

    Adição da função checar produto
    Foi adicionada no menu de interação a função checar produto, onde ele praticamente tem as mesmas funções do realizar compra, porém sem os métodos de pagamento

--- Projeto/src/application/main.java
+++ Projeto/src/application/main.java
@@ -11,10 +11,6 @@ import entities.Pagamento;
 import entities.Telefone;
 import enums.MetodosPagamento;
 
-/* Sistema de compras */
-
-/* Descrição: Sistema Criado em Java para ser feita a compra de um determinado produto */
-
 public class Main {
 
     public static void main(String[] args) {
@@ -32,7 +28,7 @@ public class Main {
                     realizarCompra(sc);
                     break;
                 case 2:
-                    // Adicione outras opções conforme necessário
+                    checarProduto(sc);
                     break;
                 case 0:
                     System.out.println("Saindo do programa. Até mais!");
@@ -49,73 +45,91 @@ public class Main {
     private static void exibirMenu() {
         System.out.println("===== MENU =====");
         System.out.println("1. Realizar Compra");
-        System.out.println("2. Outra opção");
+        System.out.println("2. Checar Produto");
         System.out.println("0. Sair");
         System.out.println("================");
         System.out.println("Escolha a opção desejada: ");
     }
 
     private static void realizarCompra(Scanner sc) {
-    // Cliente
-    System.out.println("Digite id do Cliente: ");
-    Integer idCliente = sc.nextInt();
-    System.out.println("Digite seu nome do Cliente: ");
-    String nome = sc.next(); // Use next() em vez de nextLine() para evitar problemas com quebras de linha
-    System.out.println("Digite seu email do Cliente: ");
-    String email = sc.next();
+        // Cliente
+        System.out.println("Digite id do Cliente: ");
+        Integer idCliente = sc.nextInt();
+        System.out.println("Digite seu nome do Cliente: ");
+        String nome = sc.next(); // Use next() em vez de nextLine() para evitar problemas com quebras de linha
+        System.out.println("Digite seu email do Cliente: ");
+        String email = sc.next();
 
-    Cliente cliente = new Cliente(idCliente, nome, email);
+        Cliente cliente = new Cliente(idCliente, nome, email);
 
-    // Produto
-    System.out.println("Digite o ID do Produto: ");
-    int idProd = sc.nextInt();
-    System.out.println("Digite o nome do produto: ");
-    String nomeProd = sc.next();
-    System.out.println("Digite a quantidade do Produto: ");
-    int quantProd = sc.nextInt();
-    System.out.println("Digite o preço do Produto: ");
-    double precoProd = sc.nextDouble();
+        // Produto
+        System.out.println("Digite o ID do Produto: ");
+        int idProd = sc.nextInt();
+        System.out.println("Digite o nome do produto: ");
+        String nomeProd = sc.next();
+        System.out.println("Digite a quantidade do Produto: ");
+        int quantProd = sc.nextInt();
+        System.out.println("Digite o preço do Produto: ");
+        double precoProd = sc.nextDouble();
 
-    Produto product = new Produto(idProd, nomeProd, quantProd, precoProd);
+        Produto product = new Produto(idProd, nomeProd, quantProd, precoProd);
 
-    // Pedido
-    System.out.println("Digite o nome do id: ");
-    int idPed = sc.nextInt();
-    Date dataPed = new Date(); // Renomeei para seguir convenções de nomenclatura
+        // Pedido
+        System.out.println("Digite o nome do id: ");
+        int idPed = sc.nextInt();
+        Date dataPed = new Date(); // Renomeei para seguir convenções de nomenclatura
 
-    Pedido pedido = new Pedido(idProd, product.getQuantidade(), product.getPreco(), idPed, dataPed);
+        Pedido pedido = new Pedido(idProd, product.getQuantidade(), product.getPreco(), idPed, dataPed);
 
-    System.out.println(pedido);
+        System.out.println(pedido);
 
-    // Pagamento
-    System.out.println("Digite o telefone: ");
-    int telefone = sc.nextInt();
+        // Pagamento
+        System.out.println("Digite o telefone: ");
+        int telefone = sc.nextInt();
 
-    Telefone tel = new Telefone(telefone);
-    System.out.println("Escolha o método de pagamento (1 - PIX, 2 - BOLETO, 3 - CARTÃO): ");
-    int escolha = sc.nextInt();
+        Telefone tel = new Telefone(telefone);
+        System.out.println("Escolha o método de pagamento (1 - PIX, 2 - BOLETO, 3 - CARTÃO): ");
+        int escolhaPagamento = sc.nextInt();
 
-    MetodosPagamento metodoEscolhido;
+        MetodosPagamento metodoEscolhido;
 
-    switch (escolha) {
-        case 1:
-            metodoEscolhido = MetodosPagamento.PIX;
-            break;
-        case 2:
-            metodoEscolhido = MetodosPagamento.BOLETO;
-            break;
-        case 3:
-            metodoEscolhido = MetodosPagamento.CARTAO;
-            break;
-        default:
-            throw new IllegalArgumentException("Opção de pagamento inválida");
+        switch (escolhaPagamento) {
+            case 1:
+                metodoEscolhido = MetodosPagamento.PIX;
+                break;
+            case 2:
+                metodoEscolhido = MetodosPagamento.BOLETO;
+                break;
+            case 3:
+                metodoEscolhido = MetodosPagamento.CARTAO;
+                break;
+            default:
+                throw new IllegalArgumentException("Opção de pagamento inválida");
+        }
+
+        Pagamento pagamento = new Pagamento();
+        pagamento.realizarPagamento(metodoEscolhido);
+
+        System.out.println(cliente.toString());
+        System.out.println(product.toString());
+        System.out.println(pedido.toString());
+        System.out.println(tel.toString());
     }
 
-    Pagamento pagamento = new Pagamento();
-    pagamento.realizarPagamento(metodoEscolhido);
+    private static void checarProduto(Scanner sc) {
+        // Produto
+        System.out.println("Digite o ID do Produto: ");
+        int idProd = sc.nextInt();
+        System.out.println("Digite o nome do produto: ");
+        String nomeProd = sc.next();
+        System.out.println("Digite a quantidade do Produto: ");
+        int quantProd = sc.nextInt();
+        System.out.println("Digite o preço do Produto: ");
+        double precoProd = sc.nextDouble();
 
-    System.out.println(cliente.toString());
-    System.out.println(product.toString());
-    System.out.println(pedido.toString());
-    System.out.println(tel.toString());
+        Produto product = new Produto(idProd, nomeProd, quantProd, precoProd);
+
+        // Exibir informações do produto
+        System.out.println(product.toString());
+    }
 }
